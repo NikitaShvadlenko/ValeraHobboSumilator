0ea07731154a579c51b79ca98736926c8fe64bda
Becky <Tomskrenta@mail.ru>
2020-12-03 14:35:50 +1300

    Initial Commit

---
+++ ValeraGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ValeraGame
+//
+//  Created by Nikita Shvad on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
